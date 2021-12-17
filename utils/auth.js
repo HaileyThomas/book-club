9264b1591a5410f00e13bfae63aa2f7575facc01
Hailey Thomas <haileyraebeauty@gmail.com>
2021-12-16 18:31:28 -0700

    connect server and add withAuth function

--- utils/auth.js
+++ utils/auth.js
@@ -1,1 +1,10 @@
-// AUTHGAURD FOR AUTHENTICATION WILL GO HERE
+// check for existence of a session property and redirect to login if they are logged out
+const withAuth = (req, res, next) => {
+  if (!req.session.user_id) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
