1103e949e58d5b5896bcc922802345ed4abb82de
Nathan Nelson <nathan.nelson042@gmail.com>
2021-12-16 18:26:34 -0700

    added the book model

--- models/Book.js
+++ models/Book.js
@@ -1,4 +1,34 @@
 // BOOK MODEL
+const { Model, Datatypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
-// TO DO
-// CREATE BOOK MODEL
+Books.init(
+    {
+        id: {
+            type: Datatypes.INTEGER,
+            allowNull: false,
+            unique: true
+        },
+        title: {
+            type: Datatypes.STRING,
+            allowNull: false
+        },
+        author: {
+            type: Datatypes.STRING,
+            allowNull: false
+        },
+        publication: {
+            type: Datatypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: Datatypes.STRING,
+            allowNull: false
+        }
+    }
+);
+
+
+
+
+module.exports = { Books }
