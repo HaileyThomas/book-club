cf5eafd282d52f427e828a9e0a87be31af2d0809
Hailey Thomas <haileyraebeauty@gmail.com>
2021-12-16 18:44:15 -0700

    work on index for models and their associations

--- models/index.js
+++ models/index.js
@@ -2,9 +2,29 @@
 
 // TO DO
 // CONNECT MODELS
+const User = require('./User');
+const Book = require('./Book');
+const Comment = require('./Comment');
 
 // TO DO
 // CREATE ASSOCIATIONS
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+});
+
+Book.hasMany(Comment, {
+  foreignKey: 'book_id',
+});
+
+Comment.belongsTo(Comment, {
+  as: 'Parent',
+});
+
+// not sure if it will have to be parent_id or ParentId or parentId
+Comment.hasMany(Comment, {
+  as: 'Children',
+  foreignKey: 'parent_id',
+});
 
 // export
 module.exports = { User, Book, Comment };
