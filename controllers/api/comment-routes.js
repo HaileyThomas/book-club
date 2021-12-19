a8f03e7e8ed04b87dcf6342ee13907a956191614
Hailey Thomas <haileyraebeauty@gmail.com>
2021-12-19 16:41:45 -0700

    add comment routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -7,8 +7,50 @@ const { Comment } = require('../../models');
 // authorize
 const withAuth = require('../../utils/auth');
 
-// TO DO
-// COMMENT ROUTES WILL GO HERE
+// GET ALL COMMENTS
+router.get('/', (req, res) => {
+  Comment.findAll()
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// CREATE A COMMENT
+router.post('/', withAuth, (req, res) => {
+  Comment.create({
+    comment_text: req.body.comment_text,
+    post_id: req.body.post_id,
+    // use the id from the session
+    user_id: req.session.user_id,
+  })
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+// DELETE A COMMENT BY ID
+router.delete('/:id', withAuth, (req, res) => {
+  Comment.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCommentData) => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: 'No comment found with this id!' });
+        return;
+      }
+      res.json(dbCommentData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 // export
 module.exports = router;
