18ee97a65781155a82094ff80b570a4d87717a10
Hailey Thomas <haileyraebeauty@gmail.com>
2021-12-19 16:58:14 -0700

    add homepage routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -9,8 +9,7 @@ const { Book, User, Comment } = require('../models');
 // authorize
 const withAuth = require('../utils/auth');
 
-// TO DO
-// DASHBOARD ROUTES TO GO HERE
+
 
 // export
 module.exports = router;
