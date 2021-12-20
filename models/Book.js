de0e6d5877e26f2d8f2783e5e83c36aa2f39f1ea
Nathan Nelson <nathan.nelson042@gmail.com>
2021-12-19 18:15:02 -0700

    fixed book model syntax to meet prettier standards. Going to make a dev branch from here. We will push to that from here out.

--- models/Book.js
+++ models/Book.js
@@ -3,32 +3,32 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 Books.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            unique: true
-        },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        author: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        publication: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        description: {
-            type: DataTypes.TEXT,
-            allowNull: false
-        }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    publication: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false
     }
+  }
 );
 
 
 
 
-module.exports = { Books }
+module.exports = { Books };
