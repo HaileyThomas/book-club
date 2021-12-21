bbedd2f44281eea0b462288d8bdffb30109bd4fc
Hailey Thomas <haileyraebeauty@gmail.com>
2021-12-21 13:28:06 -0700

    add edit comment form handler

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,1 +1,32 @@
 // JAVASCRIPT FOR COMMENTS HERE
+
+// EDIT COMMENT FORM HANDLER
+async function editFormHandler(event) {
+  event.preventDefault();
+  const comment_text = document.querySelector(
+    'textarea[name="edit-comment-body"]'
+  ).value;
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+  const response = await fetch(`/api/comments/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      comment_text,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard/');
+  } else {
+    alert(response.statusText);
+  }
+}
+
+// LISTENERS
+document
+  .querySelector('.edit-comment-form')
+  .addEventListener('submit', editFormHandler);
