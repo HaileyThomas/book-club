063421755638e859b8d8accdd65271579e5c98a5
Hailey Thomas <haileyraebeauty@gmail.com>
2021-12-21 13:57:55 -0700

    add delete comment form handler

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -26,7 +26,29 @@ async function editFormHandler(event) {
   }
 }
 
+// DELETE FORM HANDLER
+async function deleteFormHandler(event) {
+  event.preventDefault();
+
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+  const response = await fetch(`/api/comments/${id}`, {
+    method: 'DELETE',
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard/');
+  } else {
+    alert(response.statusText);
+  }
+}
+
 // LISTENERS
 document
   .querySelector('.edit-comment-form')
   .addEventListener('submit', editFormHandler);
+
+document
+  .querySelector('.delete-comment-btn')
+  .addEventListener('click', deleteFormHandler);
